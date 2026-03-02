9ff3e1d40545e306d5c1a6c39691a404ac4b24b5
Chris Coetzee <chris@activitar.com>
2026-03-02 16:30:17 +1000

    build: upgrade all dependencies and get tests to pass again

--- spec/dummy/config/boot.rb
+++ spec/dummy/config/boot.rb
@@ -1,3 +1,7 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
+
+# In newer rubies "logger" is now a gem
+# However despite setting a "require" on the Gemfile for 6.1, I still need to require it explicitly
+require "logger"
